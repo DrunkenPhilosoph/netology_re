a1fc4251d733ccd35f312131ca157d63d24e402c
Mushinskiy E <drunken.philosopher.icloud@gmail.com>
2024-02-01 16:20:16 +0300

    change main.py add uniqe_row script

--- main.py
+++ main.py
@@ -16,18 +16,19 @@ for row in contacts_list:
     pattern = r"\+?([7|8])\s?\(?(\d{3})\)?[\s-]?(\d{3,})\-?(\d{2,})\-?(\d{2,})\s?\(?(\w{3})?\.?\s?(\d{4})?\)?"
     new_phone = re.sub(pattern, r"+7(\2) \3-\4-\5 \6.\7", new_row[5])
     new_row[5] = new_phone
-    print(new_row)
     new_contact_list.append(new_row)
-
-
-
-
-
-
+unique_rows = {}
+for row in new_contact_list[1:]:
+    key = (row[0], row[1])
+    if key not in unique_rows:
+        unique_rows[key] = row
+    elif not unique_rows[key][-1] and row[-1]:
+        unique_rows[key][-1] = row[-1]
+unique_contact_list = [['lastname', 'firstname', 'surname', 'organization', 'position', 'phone', 'email']] + list(unique_rows.values())
 
 # TODO 2: сохраните получившиеся данные в другой файл
 # код для записи файла в формате CSV
 with open("phonebook.csv", "w", encoding="utf-8") as f:
-  datawriter = csv.writer(f, delimiter=',')
+  datawriter = csv.writer(f, delimiter=',',)
   # Вместо contacts_list подставьте свой список
-  datawriter.writerows(new_contact_list)
+  datawriter.writerows(unique_contact_list)
